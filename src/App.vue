a367a11e3fd34ef606fe8a026435fb040f82bf86
John Carlo Ricana <cier.alteir@gmail.com>
2023-12-29 17:31:10 +0800

    Init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <header class="p-3">POKE</header>
+
+  <div class="container mx-auto">
+    <RouterView />
+  </div>
+</template>
